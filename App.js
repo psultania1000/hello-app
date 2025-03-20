cb3647c45af5f6518c0a9cebfcaccd93f2c7ece5
Priyansh Sultania <psultania1000@gmail.com>
2025-03-20 20:44:05 +0530

    simple layout

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another Text, Just to see!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal!" />
       </View>
-      <Text style={styles.dummyText}>Hello World!</Text>
-      <Button title="Click Me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
+    backgroundColor: "white",
   },
-  dummyText: { margin: 28, borderWidth: 1, borderColor: "red", padding: 16 },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
 });
